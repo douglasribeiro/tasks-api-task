989c42028339816988589dd5c0c403b16a8ddb51
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2022-10-04 21:45:42 -0300

    first commit

---
+++ src/test/java/br/douglasrs/tasks/apitest/ApiTest.java
@@ -0,0 +1,58 @@
+package br.douglasrs.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class ApiTest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend/";
+	}
+
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveCriarTarefasComSucesso() {
+		RestAssured.given()
+			.body("{\r\n"
+					+ "	\"task\": \"Teste via API\",\r\n"
+					+ "	\"dueDate\": \"2030-12-30\"\r\n"
+					+ "}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naoDeveCriarTarefaKInvalida() {
+		RestAssured.given()
+			.body("{\r\n"
+					+ "	\"task\": \"Teste via API\",\r\n"
+					+ "	\"dueDate\": \"2010-12-30\"\r\n"
+					+ "}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("todo")
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+	
+}
